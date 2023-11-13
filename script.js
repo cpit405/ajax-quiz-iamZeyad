f5bf45d647f42371fd66932cb6f88028eeab50b3
Zeyad <76458361+iamZeyad@users.noreply.github.com>
2023-11-13 07:59:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ getReposButton.addEventListener('click', async () => {
     */
     // Example: https://api.github.com/users/facebook/repos
     const url = `https://api.github.com/users/${username}/repos`;
+    
     const ulElement = document.getElementById('repos-list');
     // Fetch and display the list of repositories as li elements inside the ul element.
-
+    const response = await fetch(url)
+    const data = await response.json()
+    data.forEach((repo)=> {
+        const liElement = document.createElement("li");
+        const anchorElement = document.createElement("a");
+        let a = anchorElement;
+        a.href = repo.html_url
+        a.innerHTML = repo.full_name
+        liElement.innerText = repo.full_name;
+        liElement.appendChild(a)
+        ulElement.appendChild(liElement)
+    })
+    console.log(data.full_name)
 });
 
